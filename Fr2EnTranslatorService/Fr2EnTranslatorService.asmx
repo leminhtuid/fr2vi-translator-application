931f0693d755fe2150c918a0733023281b16693a
Tu Le <leminhtuid@gmail.com>
2017-09-15 14:18:33 +0700

    Fr2EnService: initial project

---
+++ Fr2EnTranslatorService/Fr2EnTranslatorService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Fr2EnTranslatorService.asmx.cs" Class="Fr2EnTranslatorService.Fr2EnTranslatorService" %>
---
+++ Fr2EnTranslatorService/Fr2EnTranslatorService.asmx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Fr2EnTranslatorService
+{
+    /// <summary>
+    /// Summary description for Fr2EnTranslatorService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Fr2EnTranslatorService : System.Web.Services.WebService
+    {
+
+    }
+}
