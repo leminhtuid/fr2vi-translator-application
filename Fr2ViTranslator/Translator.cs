07a7e0ff4d44714459971b05f2039f998a380c79
Tu Le <leminhtuid@gmail.com>
2017-09-16 16:58:00 +0700

    Fr2ViApp: initial project included translator form

---
+++ Fr2ViTranslator/Translator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fr2ViTranslator
+{
+    public partial class Translator : Form
+    {
+        public Translator()
+        {
+            InitializeComponent();
+        }
+
+        private void Translator_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
